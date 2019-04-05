5d8288fbf27b9962043f484ae82bad3cce2fa89e
Francis Badasu <francisbadasu@Superman-MBP.mshome.net>
2019-04-05 16:33:36 +0000

    mobile responsiveness

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,49 +1,26 @@
-$(document).ready(function() {
-  $(".js-section-about").waypoint(
-    function(direction) {
-      if (direction == "down") {
-        $("nav").addClass("sticky");
-      } else {
-        $("nav").removeClass("sticky");
-      }
-    },
-    {
-      offset: "60px;"
-    }
-  );
+// $(document).ready(function() {
+//   $(".js-section-about").waypoint(
+//     function(direction) {
+//       if (direction == "down") {
+//         $("nav").addClass("sticky");
+//       } else {
+//         $("nav").removeClass("sticky");
+//       }
+//     },
+//     {
+//       offset: "60px;"
+//     }
+//   );
 
-  // scrolling
-  $(".js-scroll-to-about").click(function() {
-    $("html, body").animate(
-      { scrollTop: $(".js-section-about").offset().top },
-      1000
-    );
-  });
+// Humbergur
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
 
-  $(".js-scroll-to-project").click(function() {
-    $("html, body").animate(
-      { scrollTop: $(".js-section-project").offset().top },
-      1000
-    );
-  });
-
-  /** Mobile Nav */
-  $(".js-nav-icon").click(function() {
-    var nav = $(".js-main-nav");
-    var icon = $(".js-nav-icon i");
-
-    nav.slideToggle(200);
-
-    if (icon.hasClass("flaticon-menu-three-horizontal-lines-symbol")) {
-      icon.addClass("flaticon-cancel-music");
-      icon.removeClass("flaticon-menu-three-horizontal-lines-symbol");
-    } else {
-      icon.addClass("flaticon-menu-three-horizontal-lines-symbol");
-      icon.removeClass("flaticon-cancel-music");
-    }
-  });
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("main-nav");
 });
 
+// smooth scrolling
 function smoothScroll(ev) {
   const section = document.getElementById(ev.currentTarget.hash.substr(1));
   if (section !== null) {
