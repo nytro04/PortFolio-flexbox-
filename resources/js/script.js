b9e328833924afd02417c3ba6a71a968adf02fa1
Francis Badasu <francisbadasu@Superman-MBP.mshome.net>
2019-04-09 13:50:01 +0000

    Mobile responsive bug fix

--- resources/js/script.js
+++ resources/js/script.js
@@ -17,7 +17,7 @@ let mainNav = document.getElementById("js-menu");
 let navBarToggle = document.getElementById("js-navbar-toggle");
 
 navBarToggle.addEventListener("click", function() {
-  mainNav.classList.toggle("main-nav");
+  mainNav.classList.toggle("show");
 });
 
 // smooth scrolling
