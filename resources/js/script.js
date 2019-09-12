644f5cd7801b1b9e9ad821e20c8e1ce9e55009eb
Francis Badasu <francisbadasu@Superman-MacBook-Pro.local>
2019-09-12 16:20:32 +0000

    implemented v2 design

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,13 +12,13 @@
 //     }
 //   );
 
-// Humbergur
-let mainNav = document.getElementById("js-menu");
-let navBarToggle = document.getElementById("js-navbar-toggle");
+// Humbergur;
+// let mainNav = document.getElementById("js-menu");
+// let navBarToggle = document.getElementById("js-navbar-toggle");
 
-navBarToggle.addEventListener("click", function() {
-  mainNav.classList.toggle("show");
-});
+// navBarToggle.addEventListener("click", function() {
+//   mainNav.classList.toggle("show");
+// });
 
 // smooth scrolling
 function smoothScroll(ev) {
@@ -28,3 +28,78 @@ function smoothScroll(ev) {
     section.scrollIntoView({ behavior: "smooth" });
   }
 }
+
+var words = document.getElementsByClassName("word");
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+  splitLetters(words[i]);
+}
+
+function changeWord() {
+  var cw = wordArray[currentWord];
+  var nw =
+    currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
+  for (var i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i);
+  }
+
+  for (var i = 0; i < nw.length; i++) {
+    nw[i].className = "letter behind";
+    nw[0].parentElement.style.opacity = 1;
+    animateLetterIn(nw, i);
+  }
+
+  currentWord = currentWord == wordArray.length - 1 ? 0 : currentWord + 1;
+}
+
+function animateLetterOut(cw, i) {
+  setTimeout(function() {
+    cw[i].className = "letter out";
+  }, i * 80);
+}
+
+function animateLetterIn(nw, i) {
+  setTimeout(function() {
+    nw[i].className = "letter in";
+  }, 340 + i * 80);
+}
+
+function splitLetters(word) {
+  var content = word.innerHTML;
+  word.innerHTML = "";
+  var letters = [];
+  for (var i = 0; i < content.length; i++) {
+    var letter = document.createElement("span");
+    letter.className = "letter";
+    letter.innerHTML = content.charAt(i);
+    word.appendChild(letter);
+    letters.push(letter);
+  }
+
+  wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 4000);
+
+// Progress bar
+$(".horizontal .progress-fill span").each(function() {
+  var percent = $(this).html();
+  $(this)
+    .parent()
+    .css("width", percent);
+});
+
+$(".vertical .progress-fill span").each(function() {
+  var percent = $(this).html();
+  var pTop = 100 - percent.slice(0, percent.length - 1) + "%";
+  $(this)
+    .parent()
+    .css({
+      height: percent,
+      top: pTop
+    });
+});
